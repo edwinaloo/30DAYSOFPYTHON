d949ec6768da94d0f4b0a3ca36ecf2f50fe8d389
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-05-17 22:01:28 +0300

    day3.py completed

--- Day1/dayOne.py
+++ Day1/dayOne.py
@@ -1,11 +1,3 @@
-# ### Day 1: Python Basics
-# Start by installing Python and a text editor. Learn the basics of Python, such as: 
-#  variables, data types, Math Operators, Comments, Input() function, Print() function, The str(), int(), and float() Functions
-
-# #### Exercise
-# Simple Python Calculator Program: Write a simple program that does basic math operations including addition, subtraction, deletion, division, power, squareroot,etc,. 
-
-
 
 
 # ### Day 1: Python Basics
@@ -82,7 +74,3 @@ while True:
         print("Invalid input. Please enter a valid choice.")
 
 print("Calculator program has ended.")
-
-
-
-
---
+++ Day3/dayThree.py
@@ -0,0 +1,50 @@
+# Fibonacci: Write a program that prompts the user to 
+# enter a positive integer n, and then prints the first 
+# n Fibonacci numbers. The Fibonacci sequence starts 
+# with 0 and 1, and each subsequent number is the sum of
+#  the two preceding numbers.
+
+def fibonacci(n):
+    sequence = [0, 1]  
+    # Initialize the Fibonacci sequence with the first 
+    # two numbers
+
+    for i in range(2, n):
+        # Calculate the next Fibonacci number as the 
+        # sum of the previous two numbers
+        next_number = sequence[i-1] + sequence[i-2]
+        sequence.append(next_number)
+
+    return sequence[:n] 
+ # Return only the first n Fibonacci numbers
+
+
+# Prompt the user to enter a positive integer
+n = int(input("Enter a positive integer: "))
+
+# Call the fibonacci function and print the result
+fib_numbers = fibonacci(n)
+print("Fibonacci sequence:")
+for num in fib_numbers:
+    print(num)
+
+
+# In this program, the fibonacci function takes a parameter n representing the number of Fibonacci numbers to generate. 
+# It uses a list sequence to store the Fibonacci sequence, starting with the first two numbers 0 and 1. 
+# It then iterates from index 2 up to n and calculates each subsequent 
+# Fibonacci number by summing the previous two numbers. 
+# The function returns a slice of the sequence containing only the first n Fibonacci numbers.
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
